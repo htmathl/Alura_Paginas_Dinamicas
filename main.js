8e199e7d8096096591491d8590fb28cc584b96d3
Matheus <matheus.domingues126@gmail.com>
2023-03-27 13:00:57 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function tocaSom(idAudio) {
+    document.querySelector(idAudio).play();
+}
+
+const listaTeclas = document.querySelectorAll('.tecla');
+
+for(let contador = 0; contador < listaTeclas.length; contador++) {
+    
+    const tecla = listaTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = () => {
+        tocaSom(idAudio);
+    };
+}
